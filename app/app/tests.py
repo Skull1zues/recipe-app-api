11d743700282e889b71354730a5bd649534560f9
Skull1zues <66364726+Skull1zues@users.noreply.github.com>
2024-08-12 01:40:05 +0530

    Added wait_for_db Custom Command && fix flake8 error

--- app/app/tests.py
+++ app/app/tests.py
@@ -4,9 +4,10 @@ Sample tests
 from django.test import SimpleTestCase
 from app import calc
 
+
 class ClacTests(SimpleTestCase):
     """Test the clac module."""
 
     def test_add_numbers(self):
-        res=calc.add(5,8)
+        res = calc.add(5, 8)
         self.assertEqual(res, 13)
