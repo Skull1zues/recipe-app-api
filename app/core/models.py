911cfb8f7fa4930812801e576e08a9692641e927
Skull1zues <66364726+Skull1zues@users.noreply.github.com>
2024-08-13 02:12:11 +0530

    Configure admin dashboard and fix linting

--- app/core/models.py
+++ app/core/models.py
@@ -2,29 +2,32 @@
 Database Models
 """
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import (AbstractBaseUser,
+                                        BaseUserManager,
+                                        PermissionsMixin)
+
 
 class UserManager(BaseUserManager):
     """Manager for users"""
-    
-    def create_user(self,email,password=None, **extra_fields):
+
+    def create_user(self, email, password=None, **extra_fields):
         """Create, save and return a User"""
         if not email:
             raise ValueError('User must need a email address')
-        email=self.normalize_email(email=email)
+        email = self.normalize_email(email=email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        
+
         return user
-    
+
     def create_superuser(self, email, password):
         """Create a super user"""
-        user= self.create_user(email=email,password=password)
-        user.is_staff= True
-        user.is_superuser= True
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
         user.save(using=self._db)
-        
+
         return user
 
 
@@ -32,9 +35,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     """User in the system"""
     email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
-    is_active = models.BooleanField(default= True)
-    is_staff =models.BooleanField(default= False)
-    
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
     objects = UserManager()
-    
+
     USERNAME_FIELD = 'email'
