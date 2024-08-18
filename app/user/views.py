cacecad40fac5c4f67f58a6d1aeb2dc048879620
Skull1zues <66364726+Skull1zues@users.noreply.github.com>
2024-08-19 01:36:55 +0530

    adding TOKEN api

--- app/user/views.py
+++ app/user/views.py
@@ -2,12 +2,20 @@
 Views for the user API
 """
 from rest_framework import generics
-from user.serializers import UserSerializer
+from user.serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.views import APIView
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new useer in the system"""
     serializer_class = UserSerializer
+
+class CreateTokenView(ObtainAuthToken):
+    """ Create a auth token for user."""
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+    
 
 
 
