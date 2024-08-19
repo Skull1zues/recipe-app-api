3e036f55b3bd2cc42c98bcbaeae66a7291baf4c3
Skull1zues <66364726+Skull1zues@users.noreply.github.com>
2024-08-19 16:45:01 +0530

    User manage API(me) added

--- app/user/views.py
+++ app/user/views.py
@@ -1,7 +1,7 @@
 """
 Views for the user API
 """
-from rest_framework import generics
+from rest_framework import generics,authentication,permissions
 from user.serializers import UserSerializer, AuthTokenSerializer
 from rest_framework.views import APIView
 from rest_framework.authtoken.views import ObtainAuthToken
@@ -15,6 +15,16 @@ class CreateTokenView(ObtainAuthToken):
     """ Create a auth token for user."""
     serializer_class = AuthTokenSerializer
     renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user"""
+    serializer_class = UserSerializer
+    authentication_classes = (authentication.TokenAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def  get_object(self):
+        """Retrieve and return the authenticated user"""
+        return self.request.user
     
 
 
